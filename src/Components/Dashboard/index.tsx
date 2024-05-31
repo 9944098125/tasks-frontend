5a09a456558e9e549c6d4944cca70881714f80f6
Srinivas Akella <akellasrinivas9944@gmail.com>
2024-05-31 15:40:31 +0530

    added two more functionalities

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -5,18 +5,115 @@ import { useGetTodoQuery } from "../../Redux/Actions/todo";
 
 const Dashboard = () => {
 	const { data, refetch } = useGetTodoQuery();
-	console.log(data);
+	// console.log(data);
+	const [searchTerm, setSearchTerm] = React.useState("");
+	const [filteredTasks, setFilteredTasks] = React.useState<any[]>([]);
+	const [statusFilter, setStatusFilter] = React.useState("ALL");
+
+	const filterTodo = () => {
+		if (searchTerm === "") {
+			setFilteredTasks(data?.tasks);
+		} else {
+			const filtered = data?.tasks.filter((task: any) =>
+				task.title.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+			setFilteredTasks(filtered);
+		}
+	};
 
 	React.useEffect(() => {
 		refetch();
 	}, [refetch]);
 
+	React.useEffect(() => {
+		if (data) {
+			setFilteredTasks(data.tasks);
+		}
+	}, [data]);
+
+	React.useEffect(() => {
+		if (data) {
+			let filtered = data.tasks;
+
+			if (statusFilter !== "ALL") {
+				filtered = filtered.filter((task: any) => task.status === statusFilter);
+			}
+
+			setFilteredTasks(filtered);
+		}
+	}, [data, statusFilter]);
+
+	const handleStatusChange = (event: any) => {
+		setStatusFilter(event.target.value);
+	};
+
 	return (
 		<React.Fragment>
-			<Navbar />
-			<div className="bg-white my-10">
+			<Navbar
+				filterTodo={filterTodo}
+				searchTerm={searchTerm}
+				setSearchTerm={setSearchTerm}
+				setStatusFilter={setStatusFilter}
+			/>
+			<div className="bg-white my-10 pt-10">
+				<div className="flex justify-center items-center space-x-4 py-2 lg:hidden">
+					<input
+						type="text"
+						placeholder="search by title"
+						name="searchTerm"
+						value={searchTerm}
+						onChange={(event: any) => setSearchTerm(event?.target.value)}
+						onInput={filterTodo}
+						className="h-[45px] w-[300px] border-none outline-none bg-gray-300 rounded px-2 py-1"
+					/>
+				</div>
+				<div className="flex justify-center items-center space-x-3 lg:hidden">
+					<label
+						htmlFor="all"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">ALL</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="all"
+							type="radio"
+							name="status"
+							value="ALL"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+							defaultChecked
+						/>
+					</label>
+					<label
+						htmlFor="completed"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">COMPLETED</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="completed"
+							type="radio"
+							name="status"
+							value="COMPLETED"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+						/>
+					</label>
+					<label
+						htmlFor="pending"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">PENDING</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="pending"
+							type="radio"
+							name="status"
+							value="PENDING"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+						/>
+					</label>
+				</div>
 				<div className="w-full flex items-center flex-wrap px-5 py-5">
-					{data?.tasks.map((task: any) => {
+					{filteredTasks?.map((task: any) => {
 						return <Task task={task} key={task._id} />;
 					})}
 				</div>
--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -2,13 +2,79 @@ import React from "react";
 import logo from "../../Assets/logo.png";
 import CreateButton from "../../Utils/CreateButton";
 
-const Navbar = () => {
+type NavbarProps = {
+	filterTodo: any;
+	searchTerm: string;
+	setSearchTerm: (event: any) => void;
+	setStatusFilter: (value: string) => void;
+};
+const Navbar = (props: NavbarProps) => {
+	const { filterTodo, searchTerm, setSearchTerm, setStatusFilter } = props;
+	const handleStatusChange = (event: any) => {
+		setStatusFilter(event.target.value);
+	};
 	return (
 		<React.Fragment>
 			<nav className="bg-white fixed-top h-[70px] flex items-center justify-between px-10 py-2 shadow-lg">
 				<div className="flex items-center space-x-3 px-5">
 					<img src={logo} alt="" style={{ width: "50px", height: "50px" }} />
 					<h6 className="text-2xl text-cyan-600 font-bold">Todo</h6>
+				</div>
+				<div className="hidden lg:flex items-center space-x-4 py-2">
+					<input
+						type="text"
+						placeholder="search by title"
+						name="searchTerm"
+						value={searchTerm}
+						onChange={(event: any) => setSearchTerm(event?.target.value)}
+						onInput={filterTodo}
+						className="h-[45px] w-[300px] border-none outline-none bg-gray-300 rounded px-2 py-1"
+					/>
+				</div>
+				<div className="hidden lg:flex items-center space-x-3">
+					<label
+						htmlFor="all"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">ALL</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="all"
+							type="radio"
+							name="status"
+							value="ALL"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+							defaultChecked
+						/>
+					</label>
+					<label
+						htmlFor="completed"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">COMPLETED</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="completed"
+							type="radio"
+							name="status"
+							value="COMPLETED"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+						/>
+					</label>
+					<label
+						htmlFor="pending"
+						className="text-sm text-cyan-700 cursor-pointer border-2 border-cyan-500 rounded-lg px-3 py-1 flex items-center space-x-2">
+						<span className="radio-label">PENDING</span>
+						<span className="radio-replacement"></span>
+						<input
+							id="pending"
+							type="radio"
+							name="status"
+							value="PENDING"
+							onChange={handleStatusChange}
+							className="h-[25px] w-[25px]"
+						/>
+					</label>
 				</div>
 				<div className="icon-container">
 					<CreateButton />
